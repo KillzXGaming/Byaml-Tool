3dca6837dfaed800d6c29968b00de0db67ed7bee
KillzXGaming <millernathan35@gmail.com>
2018-02-17 13:13:50 -0500

    Add program

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ByamlEditor
+{
+    partial class ByamlTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertToBigEndianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertToLittleEndianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(759, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 195);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(221, 301);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.Visible = false;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(545, 27);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(206, 469);
+            this.propertyGrid1.TabIndex = 3;
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.convertToBigEndianToolStripMenuItem,
+            this.convertToLittleEndianToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            this.toolsToolStripMenuItem.Click += new System.EventHandler(this.toolsToolStripMenuItem_Click);
+            // 
+            // convertToBigEndianToolStripMenuItem
+            // 
+            this.convertToBigEndianToolStripMenuItem.Name = "convertToBigEndianToolStripMenuItem";
+            this.convertToBigEndianToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.convertToBigEndianToolStripMenuItem.Text = "Convert to Big Endian";
+            this.convertToBigEndianToolStripMenuItem.Click += new System.EventHandler(this.convertToBigEndianToolStripMenuItem_Click);
+            // 
+            // convertToLittleEndianToolStripMenuItem
+            // 
+            this.convertToLittleEndianToolStripMenuItem.Name = "convertToLittleEndianToolStripMenuItem";
+            this.convertToLittleEndianToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.convertToLittleEndianToolStripMenuItem.Text = "Convert to Little Endian";
+            this.convertToLittleEndianToolStripMenuItem.Click += new System.EventHandler(this.convertToLittleEndianToolStripMenuItem_Click);
+            // 
+            // ByamlTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 503);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ByamlTool";
+            this.Text = "ByamlTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertToBigEndianToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertToLittleEndianToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syroot.NintenTools.Byaml.Dynamic;
+using Syroot.BinaryData;
+using Syroot.Maths;
+using Syroot.NintenTools.Yaz0;
+using OpenTK;
+
+namespace ByamlEditor
+{
+    public partial class ByamlTool : Form
+    {
+
+        public ByamlTool()
+        {
+            InitializeComponent();
+        }
+        Dictionary<string, dynamic> byaml = null;
+
+        //Settings
+       public dynamic byteOrderLE = ByteOrder.LittleEndian;
+       public dynamic byteOrder = ByteOrder.BigEndian;
+       public dynamic SupportPaths = true;
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog d = new OpenFileDialog())
+            {
+                d.Title = "Open File";
+                d.Filter = "Binary yaml|*.byaml;*.bprm;*.szs;|All files (*.*)|*.*";
+
+                if (d.ShowDialog() == DialogResult.OK)
+                {
+                    const string message = "Not implemented yet!";
+                    const string caption = "Error";
+                    var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (byaml == null) return;
+
+            dynamic SupportPaths = true;
+
+            using (SaveFileDialog s = new SaveFileDialog())
+            {
+                s.Title = "Save File";
+                s.CheckFileExists = true;
+                s.CheckPathExists = true;
+                s.DefaultExt = "byaml";
+                s.Filter = "Binary yaml|*.byaml;*.bprm;*.szs;|All files (*.*)|*.*";
+
+             //   ByamlFile.Save("3", byaml);
+
+
+                if (s.ShowDialog() == DialogResult.OK)
+                {
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void convertToBigEndianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+
+            using (OpenFileDialog d = new OpenFileDialog())
+            {
+                d.Title = "Open File";
+                d.Filter = "Binary yaml|*.byaml;*.bprm;*.szs;|All files (*.*)|*.*";
+
+                if (d.ShowDialog() == DialogResult.OK)
+                {
+                    dynamic byaml = ByamlFile.Load(d.FileName, SupportPaths, byteOrderLE);
+
+                    ByamlFile.Save(d.FileName + ".new.byaml", byaml, SupportPaths, byteOrder);
+
+                    const string message = "Successfully converted byaml to big endian!";
+                    const string caption = "Success";
+                    var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+        }
+
+        private void convertToLittleEndianToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog d = new OpenFileDialog())
+            {
+                if (d.ShowDialog() == DialogResult.OK)
+                {
+                    dynamic byaml = ByamlFile.Load(d.FileName, SupportPaths, byteOrder);
+
+                    ByamlFile.Save(d.FileName + ".new.byaml", byaml, SupportPaths, byteOrderLE);
+
+                    const string message = "Successfully converted byaml to little endian!";
+                    const string caption = "Success";
+                    var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
